52acefc3c1c9b9c2389abc5c2675a65773d69e73
IlanArellano21 <110840319+IlanArellano22@users.noreply.github.com>
2023-01-08 20:07:11 -0600

    Cambios

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,2 +1,11 @@
 export const Sleep = (ms?: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
+
+export const omit = <T extends {}>(
+  obj: T,
+  ...omits: (keyof T)[]
+): Omit<T, keyof T> => {
+  const mapObj = new Map(Object.entries(obj));
+  for (const key of omits) mapObj.delete(key as string);
+  return Object.fromEntries(mapObj) as Omit<T, keyof T>;
+};
--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,5 +1,6 @@
-import React, { createContext } from "react";
+import React, { createContext, forwardRef } from "react";
 import { ValForm, ValFormAsync } from "@app/types";
+import { omit } from "@app/common";
 
 interface FormContext<T = any> {
   value: T | undefined;
@@ -10,12 +11,29 @@ interface FormContext<T = any> {
   props: { [key: string]: any };
 }
 
-interface FormManager {
+interface FormManager<T> {
   Form: () => JSX.Element;
-  Field: () => JSX.Element;
+  Field: <TProps extends { [k: string]: any }>(
+    props: FieldProps<TProps, T>
+  ) => JSX.Element;
 }
 
-export function createFormManager(): FormManager {
+interface FormProps {}
+
+type FieldProps<TProps extends { [k: string]: any }, T> = {
+  render: React.ComponentType<TProps>;
+  field: keyof T;
+  ref?: React.Ref<any>;
+} & (Partial<Pick<TProps, "value">> &
+  Pick<TProps, Exclude<keyof TProps, "value" | "onChange">> & {
+    onChange?: TProps["onChange"] | null;
+  });
+
+const FieldControl = forwardRef<FieldProps<any, any>>((props, ref) => {
+  return <></>;
+});
+
+export function createFormManager<T>(initial: T): FormManager<T> {
   const FormCxt = createContext<FormContext>(
     undefined as unknown as FormContext
   );
@@ -24,8 +42,10 @@ export function createFormManager(): FormManager {
     return <></>;
   }
 
-  function Field() {
-    return <></>;
+  function Field<TProps extends { [k: string]: any }>(
+    props: FieldProps<TProps, T>
+  ) {
+    return <FormCxt.Consumer>{(formctx) => <></>}</FormCxt.Consumer>;
   }
 
   return { Field, Form };
